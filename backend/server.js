d66b8fc3d42e7eead48dcca68f33b066e282801d
jt171516 <jt171516@gmail.com>
2025-03-29 19:46:27 -0500

    setup db connection in backend

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,22 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
+import pg from "pg";
 
 dotenv.config();
 
+// set up PostgreSQL connection
+const { Pool } = pg;
+const pool = new Pool({
+    user: process.env.PSQL_USER,
+    host: process.env.PSQL_HOST,
+    database: process.env.PSQL_DATABASE,
+    password: process.env.PSQL_PASSWORD,
+    port: process.env.PSQL_PORT,
+});
+
 const app = express();
-const PORT = process.env.PSQL_PORT;
+const PORT = 5000;
 
 app.use(express.json());
 app.use(cors());
