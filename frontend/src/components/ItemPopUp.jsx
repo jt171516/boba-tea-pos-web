b91ea39b24724d3a3d165d38efd5c8244a4b8a52
jt171516 <jt171516@gmail.com>
2025-03-30 19:53:38 -0500

    edited popup styling

--- frontend/src/components/ItemPopUp.jsx
+++ frontend/src/components/ItemPopUp.jsx
@@ -5,16 +5,16 @@ const ItemPopUp = ({ isOpen, onClose, item }) => {
 
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-            <div className="bg-white rounded-lg shadow-lg w-1/2 h-1/2 p-6 relative">
+            <div className="bg-base-100 rounded-lg shadow-lg w-auto h-auto p-6 m-6 relative">
                 <div className = "flex items-start space-x-4">
                     <img
                         src={item?.image || "https://s3-media0.fl.yelpcdn.com/bphoto/GBAD5WodnuFXpi3Q3CnQGw/348s.jpg"}
                         alt={item?.name || "Item Name"}
-                        className="w-35 h-35 object-cover rounded"
+                        className="w-40 h-40 object-cover rounded"
                     />
                     <div>
                         <h2 className="text-xl font-bold">{item?.name || "Item Name"}</h2>
-                        <p className="text-gray-600">
+                        <p>
                             {item?.price ? `$${item.price}` : "Price not available"} | {item?.calories ? `${item.calories} calories` : "Calories not available"}
                         </p>
                     </div>
@@ -32,29 +32,28 @@ const ItemPopUp = ({ isOpen, onClose, item }) => {
                 <div className="flex justify-between mt-4 space-x-8">
                     <div className="sizes">
                         <h3 className="text-lg font-bold mb-2">Size:</h3>
-                        <input className="join-item btn" type="radio" name="size" aria-label="Small" />
-                        <input className="join-item btn" type="radio" name="size" aria-label="Medium" />
-                        <input className="join-item btn" type="radio" name="size" aria-label="Large" />
-
+                        <input className="join-item btn m-1" type="radio" name="size" aria-label="Small" />
+                        <input className="join-item btn m-1" type="radio" name="size" aria-label="Medium" />
+                        <input className="join-item btn m-1" type="radio" name="size" aria-label="Large" />
                     </div>
 
                     <div className="ice level">
                         <h3 className="text-lg font-bold mb-2">Ice Level:</h3>
-                        <input className="join-item btn" type="radio" name="ice" aria-label="0%" />
-                        <input className="join-item btn" type="radio" name="ice" aria-label="25%" />
-                        <input className="join-item btn" type="radio" name="ice" aria-label="50%" />
-                        <input className="join-item btn" type="radio" name="ice" aria-label="75%" />
-                        <input className="join-item btn" type="radio" name="ice" aria-label="100%" />
+                        <input className="join-item btn m-1" type="radio" name="ice" aria-label="0%" />
+                        <input className="join-item btn m-1" type="radio" name="ice" aria-label="25%" />
+                        <input className="join-item btn m-1" type="radio" name="ice" aria-label="50%" />
+                        <input className="join-item btn m-1" type="radio" name="ice" aria-label="75%" />
+                        <input className="join-item btn m-1" type="radio" name="ice" aria-label="100%" />
 
                     </div>
 
                     <div className="sugar level">
                         <h3 className="text-lg font-bold mb-2">Sugar Level:</h3>
-                        <input className="join-item btn" type="radio" name="sugar" aria-label="0%" />
-                        <input className="join-item btn" type="radio" name="sugar" aria-label="25%" />
-                        <input className="join-item btn" type="radio" name="sugar" aria-label="50%" />
-                        <input className="join-item btn" type="radio" name="sugar" aria-label="75%" />
-                        <input className="join-item btn" type="radio" name="sugar" aria-label="100%" />
+                        <input className="join-item btn m-1" type="radio" name="sugar" aria-label="0%" />
+                        <input className="join-item btn m-1" type="radio" name="sugar" aria-label="25%" />
+                        <input className="join-item btn m-1" type="radio" name="sugar" aria-label="50%" />
+                        <input className="join-item btn m-1" type="radio" name="sugar" aria-label="75%" />
+                        <input className="join-item btn m-1" type="radio" name="sugar" aria-label="100%" />
                     </div>
                 </div>
             </div>
