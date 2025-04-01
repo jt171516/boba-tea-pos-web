f273f086afbee4c8f771db69e87febbd8fd0f38d
jt171516 <jt171516@gmail.com>
2025-03-31 21:24:36 -0500

    added ability to run backend and frontend at the same time

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.get("/", (req,res) => {
     res.send("Hello from the backend");
 })
 
-app.get("/item", async (req, res) => {
+app.get("/api/item", async (req, res) => {
     try {
         const result = await pool.query("SELECT * FROM item");
         res.json(result.rows);
--- frontend/src/pages/CustomerPage.jsx
+++ frontend/src/pages/CustomerPage.jsx
@@ -15,7 +15,7 @@ function CustomerPage() {
   useEffect(() => {
     const fetchItems = async () => {
       try {
-        const response = await fetch('http://localhost:5001/item');
+        const response = await fetch('http://localhost:5001/api/item');
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
