fc9e32ce8f98cd172abbb73bbddf4d0339d4b316
AsianPantsMan <casualcaseytime@gmail.com>
2025-04-14 12:09:30 -0500

    disable oauth temporarily

--- backend/auth.js
+++ backend/auth.js
@@ -9,17 +9,18 @@ passport.use(new GoogleStrategy({
     clientID:     process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: `${process.env.CURRENT_LOCALHOST}/auth/google/callback`,
+    scope: ["profile", "email"],
     passReqToCallback: true
   },
   // Authentication is successful if the user is from TAMU
   function(request, accessToken, refreshToken, profile, done) {
-    if (!profile._json.hd || profile._json.hd.toLowerCase() !== "tamu.edu") {
-      return done(null, false, { message: 'Only TAMU accounts are allowed.' });
-    }
-    else {
+    // if (!profile._json.hd || profile._json.hd.toLowerCase() !== "tamu.edu") {
+    //   return done(null, false, { message: 'Only TAMU accounts are allowed.' });
+    // }
+    // else {
       return done(null, profile);
     }
-  }
+  // }
 ));
 
 // Serialize and deserialize user information
